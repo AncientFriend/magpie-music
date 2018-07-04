d5337c73235f54a5b9117e366a743715b06d904d
Janis Christodulow <janis.christodulow@shinepowered.com>
2018-07-04 17:22:39 +0200

    fixed typos

--- helpers/commands.js
+++ helpers/commands.js
@@ -158,7 +158,7 @@ module.exports.search = async (args, message) => {
 
 module.exports.addCached = async (args, message) => {
   try {
-    const cachedValues = await cache.getCache;
+    const cachedValues = await cache.getCache();
     const normalizedCache = await getInfo(cachedValues.id);
     queue.addToQueue(normalizedCache);
   } catch (e) {
@@ -205,6 +205,7 @@ getInfo = async (data, message) => {
     let url;
     url = 'https://www.googleapis.com/youtube/v3/videos?id=' + data + '&part=contentDetails&key=' + process.env.API_KEY;
     const timeResponse = await request.get(url);
+    console.warn('TIMERESPONSE', timeResponse);
     const isoTime = timeResponse.body.items[0].contentDetails.duration;
     let duration = convertTime(isoTime);
     url = 'https://www.googleapis.com/youtube/v3/videos?id=' + data + '&key=' + process.env.API_KEY + '&part=snippet';
